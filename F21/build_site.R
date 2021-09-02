5eeff3147ac1972d7a40fe9ebcabf2d9a1e3f0ef
Olga Vitek <o.vitek@neu.edu>
2021-09-02 10:01:25 -0400

    1

--- F21/build_site.R
+++ F21/build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/olga/Documents/GitHub/CS7200/S20")
+setwd("/Users/olga/Documents/GitHub/CS7200/F21")
 
 #render your sweet site. 
 rmarkdown::render_site()
